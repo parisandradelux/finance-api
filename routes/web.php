8dfa631b6f60c064a44f1f82dbfe78e64b253fe0
Paris <parisandradelux@gmail.com>
2026-03-09 22:38:04 -0600

    docs: update README with installation steps and endpoints

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'app' => 'Finance API',
+        'version' => '1.0',
+        'status' => 'running'
+    ]);
 });
